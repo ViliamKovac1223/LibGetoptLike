a53dc06d2bb84821cdd80debde86059ab254fb89
ViliamKovac1223 <viliamkovac1223@gmail.com>
2024-08-10 11:19:50 +0200

    Add examples

---
+++ longFlagsExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using LibGetoptLike;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // Create a new GetoptLike object with flags in short format
+        GetoptLike getopt = new GetoptLike(args,
+                new GetoptArg[] {
+                    //            shortFlag longFlag        FlagType
+                    new GetoptArg("s",      "standalone",   FlagType.NoArgument),
+                    new GetoptArg("r",      "required",     FlagType.ArgumentRequired),
+                    new GetoptArg("",       "optional",     FlagType.ArgumentOptional)
+                }
+                );
+
+        // Get a list of processed arguments
+        List<GetoptArg> gArgs = getopt.gArgs;
+        // Get a list of other arguments that couldn't be processed
+        List<string> otherArgs = getopt.otherArgs;
+
+        // Go through all given flags
+        Console.WriteLine("Flags:");
+        foreach (GetoptArg gArg in gArgs)
+        {
+            switch (gArg.longFlag)
+            {
+                case "standalone":
+                    Console.WriteLine("Standalone flag (--standalone) has been used.");
+                    break;
+                case "required":
+                    Console.WriteLine($"Flag with required argument (--required) has been used with argument: \"{gArg.argument}\"");
+                    break;
+                case "optional":
+                    if (!string.IsNullOrEmpty(gArg.argument))
+                        Console.WriteLine($"Flag with optional argument (--optional) has been used with argument: \"{gArg.argument}\"");
+                    else
+                        Console.WriteLine($"Flag with optional argument (--optional) has been used with no argument");
+                    break;
+            }
+        }
+
+        // Go through all arguments that are left unprocessed
+        Console.WriteLine("Other arguments:");
+        foreach (string arg in otherArgs)
+            Console.WriteLine(arg);
+
+    }
+}
---
+++ shortFlagsExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using LibGetoptLike;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create a new GetoptLike object with flags in short format
+        GetoptLike getopt = new GetoptLike(args, "sr:o::");
+
+        // Get a list of processed arguments
+        List<GetoptArg> gArgs = getopt.gArgs;
+        // Get a list of other arguments that couldn't be processed
+        List<string> otherArgs = getopt.otherArgs;
+
+        // Go through all given flags
+        Console.WriteLine("Flags:");
+        foreach (GetoptArg gArg in gArgs)
+        {
+            switch (gArg.shortFlag)
+            {
+                case "s":
+                    Console.WriteLine("Standalone flag (-s) has been used.");
+                    break;
+                case "r":
+                    Console.WriteLine($"Flag with required argument (-r) has been used with argument: \"{gArg.argument}\"");
+                    break;
+                case "o":
+                    if (!string.IsNullOrEmpty(gArg.argument))
+                        Console.WriteLine($"Flag with optional argument (-o) has been used with argument: \"{gArg.argument}\"");
+                    else
+                        Console.WriteLine($"Flag with optional argument (-o) has been used with no argument");
+                    break;
+            }
+        }
+
+        // Go through all arguments that are left unprocessed
+        Console.WriteLine("Other arguments:");
+        foreach (string arg in otherArgs)
+            Console.WriteLine(arg);
+
+    }
+}
